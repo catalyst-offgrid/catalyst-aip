1304f8629dff4a307bd3d8e9bc3cb465c31bfa9d
Alice Rühl <alice@developmentseed.org>
2020-10-20 14:13:53 +0200

    Refactor layer control for second level

--- src/components/LayerControl.js
+++ src/components/LayerControl.js
@@ -3,58 +3,59 @@ import PropTypes from 'prop-types'
 import {
   Accordion,
   AccordionItem,
-  AccordionButton as ReachAccordionButton,
+  AccordionButton,
   AccordionPanel,
 } from '@reach/accordion'
 import '@reach/accordion/styles.css'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@reach/disclosure'
 import styled from 'styled-components'
 
 import Checkbox from './Checkbox'
-import ChevronDown from '../icons/ChevronDown'
-import ChevronUp from '../icons/ChevronUp'
+import { ChevronDown, ChevronUp, Plus, Minus } from '../icons'
 import theme from '../config/theme'
-import groups from '../config/groups'
+import uicontrols from '../config/uicontrols'
 
 const { space } = theme
-const AccordionHeader = styled.div`
-  align-items: center;
-  padding: ${space[3]}px ${space[2]}px;
-  display: flex;
-  border-top: 1px solid ${theme.colors.mediumGrey};
-  border-bottom: ${(props) =>
-    props.isOpen ? `1px solid ${theme.colors.mediumGrey}` : 0};
-`
 
-const AccordionHeading = styled.h2`
-  color: ${theme.colors.blue};
-  font-family: ${theme.fonts.heading};
-  font-size: ${theme.fontSizes[3]}pt;
-  font-weight: ${theme.fontWeights.heading};
-  margin: 0;
-`
+export default function LayerControl({ toggleLayer, layerVisibility }) {
+  const [indices, setIndices] = useState([0, 1])
+  function toggleAccordionItem(toggledIndex) {
+    if (indices.includes(toggledIndex)) {
+      setIndices(
+        indices.filter((currentIndex) => currentIndex !== toggledIndex)
+      )
+    } else {
+      setIndices([...indices, toggledIndex].sort())
+    }
+  }
 
-const AccordionSubHeading = styled.h3`
-  color: ${theme.colors.blue};
-  font-family: ${theme.fonts.heading};
-  font-size: ${theme.fontSizes[2]}pt;
-  font-weight: ${theme.fontWeights.heading};
-  margin: 0;
-`
+  return (
+    <Accordion index={indices} onChange={toggleAccordionItem}>
+      {uicontrols.map((group, index) => (
+        <FirstLevelPanel
+          key={group.id}
+          label={group.label}
+          sub={group.sub}
+          indices={indices}
+          index={index}
+          toggleLayer={toggleLayer}
+          layerVisibility={layerVisibility}
+        />
+      ))}
+    </Accordion>
+  )
+}
 
-const AccordionButton = styled(ReachAccordionButton)`
-  appearance: none;
-  background: 0;
-  border: 0;
-  padding: 0 ${space[2]}px;
-  flex-grow: 1;
-  flex-shrink: 0;
-  margin: 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
+LayerControl.propTypes = {
+  toggleLayer: PropTypes.func.isRequired,
+  layerVisibility: PropTypes.object.isRequired,
+}
 
-const ControlItem = styled.label`
+const ControlItemContainer = styled.label`
   color: ${theme.colors.blue};
   font-family: ${theme.fonts.body};
   font-size: ${theme.fontSizes[2]}pt;
@@ -69,83 +70,187 @@ const ControlItem = styled.label`
   padding: ${space[3]}px;
 `
 
-export default function LayerControl({ toggleLayer, layerVisibility }) {
-  const [indices, setIndices] = useState([0, 1])
-  function toggleItem(toggledIndex) {
-    if (indices.includes(toggledIndex)) {
-      setIndices(
-        indices.filter((currentIndex) => currentIndex !== toggledIndex)
-      )
-    } else {
-      setIndices([...indices, toggledIndex].sort())
-    }
-  }
-
+function ControlItem({ id, label, toggleLayer, layerVisibility }) {
   return (
-    <Accordion index={indices} onChange={toggleItem}>
-      {groups.map((group, index) => (
-        <AccordionItem key={group.id}>
-          <AccordionHeader isOpen={indices.includes(index)}>
-            <AccordionButton>
-              <AccordionHeading>{group.label}</AccordionHeading>
-              {indices.includes(index) ? (
-                <span
-                  style={{ marginLeft: theme.space[3] }}
-                  role='img'
-                  aria-label='chevron down'
-                >
-                  <ChevronDown color={theme.colors.blue} />
-                </span>
-              ) : (
-                <span
-                  style={{ marginLeft: theme.space[3] }}
-                  role='img'
-                  aria-label='chevron up'
-                >
-                  <ChevronUp color={theme.colors.blue} />
-                </span>
-              )}
-            </AccordionButton>
-          </AccordionHeader>
-          <AccordionPanel>
-            {Object.entries(group.sub).map(([subId, sub]) => {
-              if (sub.sub) {
-                return (
-                  <React.Fragment key={subId}>
-                    <AccordionSubHeading>{sub.label}</AccordionSubHeading>
-                    {Object.entries(sub.sub).map(([subsubId, subsub]) => (
-                      <ControlItem key={subsubId} htmlFor={subsubId}>
-                        <Checkbox
-                          id={subsubId}
-                          onChange={() => toggleLayer(subsubId)}
-                          checked={layerVisibility[subsubId]}
-                        />
-                        {subsub.label}
-                      </ControlItem>
-                    ))}
-                  </React.Fragment>
-                )
-              }
-
-              return (
-                <ControlItem key={subId} htmlFor={subId}>
-                  <Checkbox
-                    id={subId}
-                    onChange={() => toggleLayer(subId)}
-                    checked={layerVisibility[subId]}
-                  />
-                  {sub.label}
-                </ControlItem>
-              )
-            })}
-          </AccordionPanel>
-        </AccordionItem>
-      ))}
-    </Accordion>
+    <ControlItemContainer htmlFor={id}>
+      <Checkbox
+        id={id}
+        onChange={() => toggleLayer(id)}
+        checked={layerVisibility[id]}
+      />
+      {label}
+    </ControlItemContainer>
   )
 }
 
-LayerControl.propTypes = {
+ControlItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  toggleLayer: PropTypes.func.isRequired,
+  layerVisibility: PropTypes.object.isRequired,
+}
+
+const ToggleButton = styled.button`
+  appearance: none;
+  background: 0;
+  border: 0;
+  padding: 0 ${space[2]}px;
+  flex-grow: 1;
+  flex-shrink: 0;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FirstLevelHeader = styled.div`
+  align-items: center;
+  padding: ${space[3]}px ${space[2]}px;
+  display: flex;
+  border-top: 1px solid ${theme.colors.mediumGrey};
+  border-bottom: ${(props) =>
+    props.isOpen ? `1px solid ${theme.colors.mediumGrey}` : 0};
+`
+
+const FirstLevelHeading = styled.h2`
+  color: ${theme.colors.blue};
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes[3]}pt;
+  font-weight: ${theme.fontWeights.heading};
+  margin: 0;
+`
+
+function FirstLevelPanel({
+  label,
+  sub,
+  indices,
+  index,
+  toggleLayer,
+  layerVisibility,
+}) {
+  return (
+    <AccordionItem>
+      <FirstLevelHeader isOpen={indices.includes(index)}>
+        <ToggleButton as={AccordionButton}>
+          <FirstLevelHeading>{label}</FirstLevelHeading>
+          {indices.includes(index) ? (
+            <span
+              style={{ marginLeft: theme.space[3] }}
+              role='img'
+              aria-label='chevron down'
+            >
+              <ChevronDown color={theme.colors.blue} />
+            </span>
+          ) : (
+            <span
+              style={{ marginLeft: theme.space[3] }}
+              role='img'
+              aria-label='chevron up'
+            >
+              <ChevronUp color={theme.colors.blue} />
+            </span>
+          )}
+        </ToggleButton>
+      </FirstLevelHeader>
+      <AccordionPanel>
+        {Object.entries(sub).map(([subId, sub]) => {
+          if (sub.sub) {
+            return (
+              <SecondLevelPanel
+                key={subId}
+                label={sub.label}
+                sub={sub.sub}
+                toggleLayer={toggleLayer}
+                layerVisibility={layerVisibility}
+              />
+            )
+          }
+
+          return (
+            <ControlItem
+              key={subId}
+              id={subId}
+              label={sub.label}
+              toggleLayer={toggleLayer}
+              layerVisibility={layerVisibility}
+            />
+          )
+        })}
+      </AccordionPanel>
+    </AccordionItem>
+  )
+}
+
+FirstLevelPanel.propTypes = {
+  label: PropTypes.string.isRequired,
+  sub: PropTypes.object.isRequired,
+  indices: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  index: PropTypes.number.isRequired,
+  toggleLayer: PropTypes.func.isRequired,
+  layerVisibility: PropTypes.object.isRequired,
+}
+
+const SecondLevelHeader = styled(FirstLevelHeader)`
+  border-top: 1px solid ${theme.colors.white};
+  border-bottom: ${(props) =>
+    props.isOpen ? `1px solid ${theme.colors.white}` : 0};
+  background-color: ${theme.colors.lightGrey};
+`
+
+const SecondLevelHeading = styled.h3`
+  color: ${theme.colors.blue};
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes[1]}pt;
+  font-weight: ${theme.fontWeights.heading};
+  text-transform: uppercase;
+  margin: 0;
+`
+
+function SecondLevelPanel({ label, sub, toggleLayer, layerVisibility }) {
+  const [isOpen, setOpen] = useState(true)
+
+  return (
+    <Disclosure open={isOpen} onChange={() => setOpen(!isOpen)}>
+      <SecondLevelHeader isOpen={isOpen}>
+        <ToggleButton as={DisclosureButton}>
+          <SecondLevelHeading>{label}</SecondLevelHeading>
+          {isOpen ? (
+            <span
+              style={{ marginLeft: theme.space[3] }}
+              role='img'
+              aria-label='minus'
+            >
+              <Minus color={theme.colors.blue} />
+            </span>
+          ) : (
+            <span
+              style={{ marginLeft: theme.space[3] }}
+              role='img'
+              aria-label='plus'
+            >
+              <Plus color={theme.colors.blue} />
+            </span>
+          )}
+        </ToggleButton>
+      </SecondLevelHeader>
+      <DisclosurePanel>
+        {Object.entries(sub).map(([subId, sub]) => (
+          <ControlItem
+            key={subId}
+            id={subId}
+            label={sub.label}
+            toggleLayer={toggleLayer}
+            layerVisibility={layerVisibility}
+          />
+        ))}
+      </DisclosurePanel>
+    </Disclosure>
+  )
+}
+
+SecondLevelPanel.propTypes = {
+  label: PropTypes.string.isRequired,
+  sub: PropTypes.object.isRequired,
   toggleLayer: PropTypes.func.isRequired,
   layerVisibility: PropTypes.object.isRequired,
 }
