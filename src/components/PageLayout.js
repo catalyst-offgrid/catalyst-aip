64b22e22bad5b3ed6bec814426e67213582efd81
Alice Rühl <alice@developmentseed.org>
2020-10-27 14:22:13 +0100

    Add Info page

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -4,8 +4,9 @@ import styled from 'styled-components'
 
 import NavigationBar from './NavigationBar'
 import theme from '../config/theme'
+import CATALYST_BLUE from '../../img/CATALYST_BLUE.png'
 
-const { space } = theme
+const { space, colors } = theme
 
 const PageContainer = styled.div`
   height: 100%;
@@ -39,4 +40,66 @@ PageLayout.propTypes = {
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
   ]),
+}
+
+export const Introduction = styled.div`
+  grid-column: 1 / span 5;
+`
+
+export const Tagline = styled.span`
+  color: ${colors.primary};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes[3]}pt;
+  font-weight: ${theme.fontWeights.bold};
+  text-transform: uppercase;
+`
+
+export const PageTitle = styled.h1`
+  color: ${colors.primary};
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes[5]}pt;
+  font-weight: ${theme.fontWeights.heading};
+
+  margin-top: ${space[3]}px;
+  margin-bottom: ${space[4]}px;
+`
+
+export const Paragraph = styled.p`
+  color: ${colors.text};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes[3]}pt;
+  font-weight: ${theme.fontWeights.body};
+
+  margin-top: ${space[4]}px;
+  margin-bottom: ${space[3] + space[4]}px;
+`
+
+const LogoContainer = styled.figure`
+  grid-column: 1 / span 5;
+  align-self: end;
+  margin: 0;
+
+  display: flex;
+  flex-direction: column;
+`
+
+const Caption = styled.figcaption`
+  color: ${colors.primary};
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes[3]}pt;
+  font-weight: ${theme.fontWeights.body};
+  text-transform: uppercase;
+
+  margin: ${space[3]}px 0;
+`
+
+export const Logo = ({ withTagline }) => (
+  <LogoContainer>
+    {withTagline && <Caption>An Innovation Of</Caption>}
+    <img alt='CATALYST Off Grid Advisors' src={CATALYST_BLUE} width={400} />
+  </LogoContainer>
+)
+
+Logo.propTypes = {
+  withTagline: PropTypes.bool,
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,44 +3,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import PageLayout from '../components/PageLayout'
+import PageLayout, {
+  Introduction,
+  Tagline,
+  PageTitle,
+  Paragraph,
+  Logo,
+} from '../components/PageLayout'
 import theme from '../config/theme'
 
-import CATALYST_BLUE from '../../img/CATALYST_BLUE.png'
-
 const { colors, space } = theme
-
-const Introduction = styled.div`
-  grid-column: 1 / span 5;
-`
-
-const Tagline = styled.span`
-  color: ${colors.primary};
-  font-family: ${theme.fonts.body};
-  font-size: ${theme.fontSizes[3]}pt;
-  font-weight: ${theme.fontWeights.bold};
-  text-transform: uppercase;
-`
-
-const PageTitle = styled.h1`
-  color: ${colors.primary};
-  font-family: ${theme.fonts.heading};
-  font-size: ${theme.fontSizes[5]}pt;
-  font-weight: ${theme.fontWeights.heading};
-
-  margin-top: ${space[3]}px;
-  margin-bottom: ${space[4]}px;
-`
-
-const Paragraph = styled.p`
-  color: ${colors.text};
-  font-family: ${theme.fonts.body};
-  font-size: ${theme.fontSizes[3]}pt;
-  font-weight: ${theme.fontWeights.body};
-
-  margin-top: ${space[4]}px;
-  margin-bottom: ${space[3] + space[4]}px;
-`
 
 const Actions = styled.div`
   display: inline-flex;
@@ -67,25 +39,6 @@ const PrimaryButton = styled(Link)`
 const SecondaryButton = styled(PrimaryButton)`
   background-color: ${colors.background};
   color: ${colors.primary};
-`
-
-const Caption = styled.figcaption`
-  color: ${colors.primary};
-  font-family: ${theme.fonts.body};
-  font-size: ${theme.fontSizes[3]}pt;
-  font-weight: ${theme.fontWeights.body};
-  text-transform: uppercase;
-
-  margin: ${space[3]}px 0;
-`
-
-const Logo = styled.figure`
-  grid-column: 1 / span 5;
-  align-self: end;
-  margin: 0;
-
-  display: flex;
-  flex-direction: column;
 `
 
 const Image = styled.figure`
@@ -119,11 +72,7 @@ export default function Home({ config }) {
           </SecondaryButton>
         </Actions>
       </Introduction>
-      <Logo>
-        <Caption>An Innovation Of</Caption>
-        <img alt='CATALYST Off Grid Advisors' src={CATALYST_BLUE} width={400} />
-      </Logo>
-
+      <Logo withTagline />
       <Image url={config.imageUrl} />
     </PageLayout>
   )
