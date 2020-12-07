4d9f8a39a93c2bec26644e341671d6dcd15b1632
iandmuir <75480955+iandmuir@users.noreply.github.com>
2020-12-07 13:25:35 +0300

    Update uicontrols.js
    Made dispensaries more visible

--- src/config/ke/uicontrols.js
+++ src/config/ke/uicontrols.js
@@ -423,7 +423,7 @@ export default [
             id: 'health_type4',
             label: 'Dispensaries',
             defaultVisibility: false,
-            legend: { type: 'dot', color: colors.accent4 },
+            legend: { type: 'dot', color: colors.accent5 },
             layerIds: ['health_type4'],
             info: '',
           },
