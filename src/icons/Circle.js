074a62ed9ac44a5ca02a3db28404981db013b988
Joseph Semrai <josephsemrai@gmail.com>
2021-03-03 03:20:40 -0500

    Update Circle.js

--- src/icons/Circle.js
+++ src/icons/Circle.js
@@ -10,10 +10,6 @@ const Circle = ({ color = 'none' }) => (
     viewBox='0 0 16 16'
   >
     <circle width='16' height='16' fill={color} />
-    {/* <path
-      fill={color}
-      d='M2,9.014L3.414,7.6L6.004,10.189L12.593,3.6L14.007,5.014L6.003,13.017L2,9.014Z'
-    /> */}
   </svg>
 )
 
