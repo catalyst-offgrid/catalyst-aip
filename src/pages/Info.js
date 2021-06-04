70225fde1222cecb01762add33939107f01c2293
iandmuir <75480955+iandmuir@users.noreply.github.com>
2021-06-04 21:43:34 +0300

    Full addition of other resources

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -56,14 +56,52 @@ export default function Info({ siteAcronym, theme }) {
         </Paragraph>
 
         <Paragraph>
+          <a href="https://fraym.io/">Fraym</a> was built to help fast-growing companies, development 
+          organizations, and governments succeed in places where data has been traditionally hard to access. 
+          Using the latest geospatial technology, Fraym delivers unprecedented, localized information about 
+          population characteristics and behaviors.
+        </Paragraph>
+
+         <Paragraph>
+          <a href="https://www.energyaccessexplorer.org/">Energy Access Explorer</a> is an online, open-source, interactive 
+          platform that synthesizes and analyzes more than 25 geographic data sets (per geography) on both energy 
+          supply and demand. It is a multi-criteria analysis tool that uses location-specific resource availability
+          and infrastructure data to represent energy supply. It also incorporates demographic data and data on 
+          social and productive uses to visualize demand for energy services. Together, these supply and demand
+          indicators enable more comprehensive energy planning. Spatial analysis tools, including multi-criteria 
+          analysis, overlays, filters and buffer zones, help users identify and prioritize areas where energy access 
+          can be expanded.
+        </Paragraph>
+
+         <Paragraph>
+          <a href="https://electrifynow.energydata.info/">Global Electrification Platform</a> is an open access, interactive, 
+          online platform that allows for an overview of electrification investment scenarios for a selection of countries. 
+          The scenarios present pathways for achieving universal electricity access, split into an intermediate strategy 
+          for 2025 and full electrification by 2030.
+        </Paragraph>
+
+       <Paragraph>
+          <a href="https://www.kenya-atlas.org/interactive/index.html">Socio-Economic Atlas of Kenya</a> features high-resolution 
+          spatial depictions and analyses of data collected in the 2009 Kenya Population and Housing Census. The combination of 
+          geographic and socio-economic data enables policymakers at all levels, development experts, and other interested readers 
+          to gain a spatial understanding of dynamics affecting Kenya.
+        </Paragraph>
+
+        <Paragraph>
+          <a href="https://www.villagedata.io/">Village Data Analytics</a> is an earth observation 
+          and AI-powered custom software that enables data-driven investment, business and policy decisions in rural villages in 
+            Africa and Asia.
+        </Paragraph>
+
+
+        <Paragraph>
           For any inquiries and potential collaboration, including dataset
           contributions, please contact the development team at{' '}
           <a href='mailto:aip@catalystoffgrid.com'>
             Catalyst Off-Grid Advisors
           </a>
         </Paragraph>
-        {/* TODO: Add point of contact  */}
-      </InfoBlock>
+        </InfoBlock>
 
       <CatalystLogo withTagline />
 
